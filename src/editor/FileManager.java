b0288aaac973ec58bc8c69cac81bc80eacded6e3
Payam <payam85ar@gmail.com>
2025-04-27 17:59:40 +0330

    Add open & save & new file

--- src/editor/FileManager.java
+++ src/editor/FileManager.java
@@ -1,13 +1,28 @@
 package editor;
 
 import javax.swing.*;
+import java.io.*;
 
 public class FileManager {
     public static void openFile(TextEditor textEditor, JTextArea textArea) {
         JFileChooser fileChooser = new JFileChooser();
         if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
+            File file = fileChooser.getSelectedFile();
+
+            textEditor.currentFile = file;
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+
+                String data;
+                while ((data = br.readLine()) != null) {
+                    textArea.append(data + "\n");
+                }
+
+                br.close();
+            } catch (Exception e) {
+                textArea.append(e.getMessage());
+            }
         }
     }
 
@@ -15,15 +30,35 @@ public class FileManager {
         if (textEditor.currentFile == null){
             JFileChooser fileChooser = new JFileChooser();
             if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-                //TODO
+                File file = fileChooser.getSelectedFile();
+
+                try {
+                    BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+
+                    bw.write(textArea.getText());
+
+                    bw.close();
+                } catch (Exception e) {
+                    textArea.append(e.getMessage());
+                }
             }
         }
         else{
-            //TODO
+            try {
+                BufferedWriter bw = new BufferedWriter(new FileWriter(textEditor.currentFile, true));
+
+                bw.write(textArea.getText());
+
+                bw.close();
+            } catch (Exception e) {
+                textArea.append(e.getMessage());
+            }
         }
     }
 
     public static void newFile(TextEditor textEditor, JTextArea textArea) {
-        //TODO
+        textEditor.currentFile = null;
+
+        textArea.setText("");
     }
 }
